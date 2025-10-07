20f54199ab057f22726a5869bad96cb0e4516558
Kevin Luvizotto <kevin.luvizotto@hotmail.com>
2025-10-07 12:23:18 -0300

    Updated config.js

--- src/FCG.Solution.Api/wwwroot/config.js
+++ src/FCG.Solution.Api/wwwroot/config.js
@@ -1,3 +1,4 @@
-﻿const apiUsers = "https://fcg-apim-fiap-klztt.azure-api.net/users";
+﻿// config.js
+const apiUsers = "https://fcg-apim-fiap-klztt.azure-api.net/users";
 const apiGames = "https://fcg-apim-fiap-klztt.azure-api.net/games";
 const apiPayments = "https://fcg-apim-fiap-klztt.azure-api.net/payments";
